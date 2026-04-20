dfe8c8c0469a5bc0d5905a4b6bcc03267bcff1f3
Sophia <sophiafranca2204@gmail.com>
2026-04-19 23:54:43 -0300

    mudando

--- atv35.c
+++ atv35.c
@@ -1,5 +1,19 @@
 #include <stdio.h> 
 int main()
 {
-    
+    float distancia, valor;
+
+    printf("Digite a distancia(km): ");
+    scanf("%f", &distancia);
+
+    if (distancia <= 200){
+        valor = distancia * 0.50;
+    }
+    else {
+        valor = distancia * 0.45;
+    }
+
+    printf("Valor da passagem: R$ %.2f\n", valor);
+
+    return 0;
 }
